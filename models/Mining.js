82a4aed08018ac1853d0815d3d95ad47821f014a
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-07 09:45:24 +0530

    Update Mining.js

--- models/Mining.js
+++ models/Mining.js
@@ -1,1 +1,10 @@
 
+const mongoose = require('mongoose');
+
+const miningSchema = mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  minedPoints: { type: Number, required: true },
+  date: { type: Date, required: true, default: Date.now },
+});
+
+module.exports = mongoose.model('Mining', miningSchema);

1346455aaf21d91c210733190f5a0398e32792a3
royak47 <52905147+royak47@users.noreply.github.com>
2024-12-10 20:52:35 +0530

    Delete models directory

--- models/Mining.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const miningSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  minedPoints: { type: Number, required: true },
-  date: { type: Date, default: Date.now },
-});
-
-export default mongoose.models.Mining || mongoose.model('Mining', miningSchema);
